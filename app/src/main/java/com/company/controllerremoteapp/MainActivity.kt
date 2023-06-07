836ca78bbd33eba5c9e01f3087fac46bc39a508d
Ezequiel Biglia <ezequielbiglia12@gmail.com>
2023-06-07 17:09:36 -0300

    Task - VieBinding - se agrega viewBinding

--- app/src/main/java/com/company/controllerremoteapp/MainActivity.kt
+++ app/src/main/java/com/company/controllerremoteapp/MainActivity.kt
@@ -2,10 +2,21 @@ package com.company.controllerremoteapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.company.controllerremoteapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+       /* binding.textView.text = "lulululu"
+        binding.textView.setTextColor(resources.getColor(R.color.pink))*/
+
     }
+
+
 }
